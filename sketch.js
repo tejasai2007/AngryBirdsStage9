ee6f3843d6c747da13055c06bfc2e92aa71b0719
tejasai2007 <tejasaicharan55@gmail.com>
2020-11-25 13:57:26 +0530

    debugged

--- sketch.js
+++ sketch.js
@@ -93,13 +93,14 @@ function mouseReleased(){
 }
 
 function keyPressed(){
-    if(keyCode === 32){
+    if(keyCode === 32 && bird.body.speed <1 ){
         bird.trajectory = [] ;
         Matter.Body.setPosition(bird.body, {x: 200 , y: 50});
         slingshot.attach(bird.body);
 
 
     }
+    
 }
 
 async function getBackgroundImg(){
